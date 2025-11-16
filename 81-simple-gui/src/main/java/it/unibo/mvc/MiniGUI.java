2729de0ef96f6baa4fae58fa03514e2adcc3bb32
Pietro Dapporto <dapportopietro@gmail.com>
2025-11-16 17:53:38 +0100

    compleated exercise 81

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -6,7 +6,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-// import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import java.awt.BorderLayout;
